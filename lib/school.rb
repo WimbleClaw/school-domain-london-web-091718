4fbbcd7596afc52ec81c579da32188f8050ad680
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-19 12:24:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+  end
+
+  roster = {}
+
+  def add_student=(name, grade)
+    @name = name 
+    @grade = grade
+  end
+
+  def add_student
+        roster[@grade] = @name
+  end
+end
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.roster
