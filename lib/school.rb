a72f40e6673bb2df7183fa54cbef6711174a5340
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-19 12:26:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,14 +6,11 @@ class School
 
   roster = {}
 
-  def add_student=(name, grade)
-    @name = name 
+  def add_student(name, grade)
+    @name = name
     @grade = grade
   end
 
-  def add_student
-        roster[@grade] = @name
-  end
 end
 
 school = School.new("Bayside High School")
